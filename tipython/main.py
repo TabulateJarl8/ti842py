bb892015483b19a7b4e1f54b94b2302d82b7ed8c
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-02-03 23:03:45 -0500

    basic transpiler functionality

---
+++ tipython/main.py
@@ -0,0 +1,41 @@
+import argparse
+import pyparsing
+from parser import BasicParser
+
+def transpile(infile, outfile):
+
+	# Read infile
+	with open(infile, 'r') as f:
+		file_lines = [line.strip() for line in f.readlines()]
+
+	pythonCode = BasicParser(file_lines).toPython()
+
+	# Write to outfile
+	if outfile == "stdout":
+		print("\n".join(pythonCode))
+	else:
+		with open(outfile, 'w') as f:
+			for line in pythonCode:
+				f.write(line + "\n")
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'-o',
+		required=False,
+		default='stdout',
+		help="Optional output file to write to. Defaults to standard out."
+		)
+	parser.add_argument(
+		'-i',
+		required=True,
+		help="Input file."
+		)
+
+	args = parser.parse_args()
+
+	transpile(args.i, args.o)
+
+
+if __name__ == "__main__":
+	main()
---
+++ tipython/parser.py
@@ -0,0 +1,143 @@
+import re
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.WARNING)
+
+def closeOpen(string):
+	newString = ""
+	# Function for closing open quotation marks/parenthesis
+	open = False
+	closingChar = ""
+	for i, c in enumerate(string):
+		if i + 1 == len(string) and c != closingChar:
+			# Last character and current character does not close
+			if open == True:
+				# Append open character to string
+				c += closingChar
+		elif c == "," and open == True:
+			# Reached a comma with open character
+			newString += closingChar
+		elif open == True and c == closingChar:
+			# Closing character reached
+			open = False
+			closingChar = ""
+		elif open == False and c == "\"":
+			# Opening quote
+			open = True
+			closingChar = "\""
+		elif open == False and c == "(":
+			# Opening parenthesis
+			open = True
+			closingChar = ")"
+		newString += c
+	return newString
+
+
+
+class BasicParser(object):
+	def __init__(self, basic):
+		if isinstance(basic, list):
+			self.basic = basic
+		elif isinstance(basic, str):
+			self.basic = [line.strip() for line in basic.split("\n")]
+		else:
+			raise TypeError("basic must be list or str, not {}".format(str(type(basic))))
+
+		# Utility Functions
+		self.IMPORTS = ["import os", ""]
+		self.CLEAR = ["def clear():", "\tif os.system(\"clear\") != 0:", "\t\tif os.system(\"cls\") != 0:", "\t\t\tprint(\"Clearing the screen is not supported on this device\")", ""]
+
+	def toPython(self):
+		pythonCode = []
+		# Add utility functions
+		pythonCode += self.IMPORTS
+		pythonCode += self.CLEAR
+
+		indentLevel = 0
+		# indentIncrease increases the indent on the next line
+		indentIncrease = False
+		# indentDecrease decreases the indent on the current line
+		indentDecrease = False
+		skipLine = False
+		for index, line in enumerate(self.basic):
+			statement = ""
+			if line.startswith("\""):
+				# Comments
+				statement = "# " + line.lstrip("\"")
+			elif skipLine == True:
+				skipLine = False
+				continue
+			# Disp
+			elif line.startswith("Disp "):
+				statement = re.search("Disp (.*[^)])", line).groups(1)[0]
+				statement = "print(" + closeOpen(statement) + ", sep=\"\")"
+			# Variables
+			elif "->" in line or "→" in line:
+				statement = re.split("->|→", line)
+				statement.reverse()
+				statement = " = ".join(statement)
+			# If
+			elif line.startswith("If "):
+				statement = "if " + line.lstrip("If ")
+				statement = statement.replace("=", " == ")
+				statement = statement + ":"
+				indentIncrease = True
+			elif line == "Then":
+				continue
+			# Elif
+			elif line == "Else" and self.basic[index + 1].startswith("If"):
+				statement = "elif " + self.basic[index + 1].lstrip("If ")
+				statement = statement.replace("=", " == ")
+				statement = statement + ":"
+				indentDecrease = True
+				indentIncrease = True
+				skipLine = True
+			# Else
+			elif line == "Else" and not (self.basic[index + 1].startswith("If")):
+				statement = "else:"
+				indentDecrease = True
+				indentIncrease = True
+			elif line == "ClrHome":
+				statement = "clear()"
+			elif line == "End":
+				indentDecrease = True
+			# Input
+			elif line.startswith("Input"):
+				statement = line.split(",")
+				statement = statement[1] + " = input(" + closeOpen(statement[0][6:]) + ")"
+			# For loop
+			elif line.startswith("For"):
+				args = line[3:].strip("()").split(",")
+				statement = "for " + args[0] + " in range(" + args[1] + ", " + args[2]
+				if len(args) == 4:
+					statement += ", " + args[3]
+				statement += "):"
+				indentIncrease = True
+			# While loop
+			elif line.startswith("While "):
+				statement = "while " + line[6:].replace("=", " == ") + ":"
+				indentIncrease = True
+			# Repeat loop (tests at bottom)
+			elif line.startswith("Repeat "):
+				statement = ["firstPass = True", "while firstPass == True or " + line[7:].replace("=", " == ") + ":", "\tfirstPass = False"]
+				indentLevel += 1
+
+			else:
+				statement = "# UNKNOWN INDENTIFIER: {}".format(line)
+				logger.warning("Unknown indentifier on line %s", index)
+
+			if indentDecrease == True:
+				indentLevel -= 1
+				indentDecrease = False
+
+			if isinstance(statement, str):
+				pythonCode.append("\t"*indentLevel + statement)
+			elif isinstance(statement, list):
+				pythonCode.extend(statement)
+
+			if indentIncrease == True:
+				indentLevel += 1
+				indentIncrease = False
+
+		return pythonCode
