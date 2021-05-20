6972181e8f02d30a11e02d5a6e80e9fd6ce1784a
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-05-19 20:58:56 -0400

    fix bugs

--- ti842py/tiParser.py
+++ ti842py/tiParser.py
@@ -25,7 +25,7 @@ def menu(title, args):
 
 	ifstmt = ["d = dialog.Dialog()", "menu = d.menu(\"" + title + "\", choices=" + str(choices) + ")"]
 	for key in args:
-		ifstmt.extend(["if menu[1] == \"" + str(key) + "\":", "\tgoto .lbl" + str(args[key])])
+		ifstmt.extend(["if menu[1] == \"" + str(key) + "\":", "\tclear()", "\tgoto .lbl" + str(args[key])])
 
 	return ifstmt
 
@@ -84,10 +84,10 @@ class TIBasicParser(object):
 	def convertLine(self, index, line):
 		statement = ""
 		# TODO: Make rules for :, dont fully understand it yet
-		if line.startswith("\""):
+		# if line.startswith("\""):
 			# Comments
-			statement = "# " + line.lstrip("\"")
-		elif self.skipLine > 0:
+			# statement = "# " + line.lstrip("\"")
+		if self.skipLine > 0:
 			self.skipLine -= 1
 			return None
 		elif line == "":
@@ -164,7 +164,7 @@ class TIBasicParser(object):
 			self.UTILS["toNumber"]["enabled"] = True
 		# For loop
 		elif line.startswith("For"):
-			args = line[3:].strip("()").split(",")
+			args = line[4:].strip("()").split(",")
 			statement = "for " + args[0] + " in range(" + args[1] + ", " + args[2]
 			if len(args) == 4:
 				statement += ", " + args[3]
@@ -247,6 +247,7 @@ class TIBasicParser(object):
 				options.extend([tiMenu[i].strip(" \""), tiMenu[i + 1].strip(" \"")])
 			statement = menu(title, options)
 			self.UTILS["menu"]["enabled"] = True
+			self.UTILS['clear']['enabled'] = True
 		# Line(
 		elif line.startswith('Line('):
 			statement = closeOpen(line.replace('Line(', 'draw.line('))
@@ -339,7 +340,7 @@ class TIBasicParser(object):
 			self.UTILS["getKey"]["enabled"] = True
 		if "[theta]" in ' '.join(statement):
 			# Replace [theta] with theta if [theta] is not inside of quotes
-			statement = [re.sub(r'(?!\B"[^"]*)\[theta\](?![^"]*"\B)', "theta", item) for item in statement]
+			statement = [item.replace('[theta]', "theta") for item in statement]
 		if "^" in ' '.join(statement):
 			# Convert every ^ not in a string to **
 			statement = [re.sub(r'(?!\B"[^"]*)\^(?![^"]*"\B)', "**", item) for item in statement]
@@ -370,6 +371,9 @@ class TIBasicParser(object):
 			self.UTILS['getDateTime']['enabled'] = True
 		if 'remainder(' in ' '.join(statement):
 			statement = [re.sub(r'remainder\(([^\)]+)\)', lambda m: m.group(1).replace(' ', '').split(',')[0] + ' % ' + m.group(1).replace(' ', '').split(',')[1], item) for item in statement]
+
+		if 'dim(' in ' '.join(statement):
+			statement = [item.replace('dim(', 'len(') for item in statement]
 
 		if re.search(r'l[1-6]\([0-9A-Za-z]+\)', ' '.join(statement)):
 			# List subscription
